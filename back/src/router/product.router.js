e27926352b453904fbdb77e185b13bccb53dca5b
altaparro <matiasaltaparro@hotmail.com>
2023-10-20 14:56:46 -0300

    modificando validaciones

--- back/src/router/product.router.js
+++ back/src/router/product.router.js
@@ -4,17 +4,24 @@ const Products = require("../model/product.model");
 const productosController = require("../controllers/productos.controller")
 const { request } = require("express");
 const app = require("../app/app");
-
 const verificarToken = require("../middleware/authMiddleware").verificarToken;
+const obtenerPermisos = require('../permisos/permisos');
 
-productoRouter.get("/products", verificarToken, productosController.obtenerTodosLosProductos);
 
-productoRouter.get("/products/:product_id", verificarToken, productosController.obtenerProductoPorID);
+const permisoCrear = "crearProducto"; // aca defino una constante con el nombre del controlador de la ruta, y la envio a permisos.js por parametro
+const permisoActualizar = "actualizarProducto";
+const permisoObtenerTodos = "obtenerTodosLosProductos";
+const permisoObtenerPorID = "obtenerProductoPorID";
+const permisoEliminar = "eliminarProducto";
 
-productoRouter.post("/products", verificarToken, productosController.crearProducto);
+productoRouter.get("/products", verificarToken, obtenerPermisos(permisoObtenerTodos), productosController.obtenerTodosLosProductos);
+
+productoRouter.get("/products/:product_id", verificarToken, obtenerPermisos(permisoObtenerPorID), productosController.obtenerProductoPorID);
+
+productoRouter.post("/products", verificarToken, obtenerPermisos(permisoCrear), productosController.crearProducto);
  
-productoRouter.put("/products/:product_id", verificarToken, productosController.actualizarProducto);
+productoRouter.put("/products/:product_id", verificarToken, obtenerPermisos(permisoActualizar), productosController.actualizarProducto);
 
-productoRouter.delete("/products/:product_id", verificarToken, productosController.eliminarProducto)
+productoRouter.delete("/products/:product_id", verificarToken, obtenerPermisos(permisoEliminar), productosController.eliminarProducto)
 
 module.exports = productoRouter;
